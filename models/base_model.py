2d425b9179ecc66bd9e82f711f818cb525af8813
Maksymilian Bilski <maksymilian.bilski@nokia.com>
2025-01-06 11:52:47 +0100

    Correct the prediction evaluation

--- models/base_model.py
+++ models/base_model.py
@@ -5,14 +5,14 @@ import torch.nn as nn
 class GMF(nn.Module):
     def __init__(self, n_users, n_items, embedding_dim=8):
         super(GMF, self).__init__()
-        
+
         self.user_embedding = nn.Embedding(num_embeddings=n_users, embedding_dim=embedding_dim)
         self.item_embedding = nn.Embedding(num_embeddings=n_items, embedding_dim=embedding_dim)
-        
+
     def forward(self, user_input, item_input):
         user_embedded = self.user_embedding(user_input)
         item_embedded = self.item_embedding(item_input)
-        
-        prediction = (user_embedded * item_embedded).sum(dim=1) 
+
+        prediction = (user_embedded * item_embedded).sum(dim=1)
 
         return prediction.squeeze()
