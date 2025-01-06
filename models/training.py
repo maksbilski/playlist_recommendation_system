8c19050be457d6b4871e975eaa5e09214c45344b
Maksymilian Bilski <maksymilian.bilski@nokia.com>
2025-01-06 11:53:49 +0100

    Change the train method so it uses AdamW with weight decay and add loading training data to torch device

--- models/training.py
+++ models/training.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 
@@ -8,15 +9,20 @@ class GMFTrainer:
         self.train_loader = train_loader
         self.device = device
 
-    def train(self, epochs, learning_rate):
+    def train_with_weight_decay(self, epochs, learning_rate, weight_decay=0.01):
         self.model = self.model.to(self.device)
         criterion = nn.MSELoss()
-        optimizer = optim.SGD(self.model.parameters(), lr=learning_rate)    
+        optimizer = optim.AdamW(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
         for epoch in range(epochs):
             self.model.train()
             total_loss = 0
             
             for user_ids, item_ids, labels in self.train_loader:
+                user_ids = user_ids.to(self.device)
+                item_ids = item_ids.to(self.device)
+                labels = labels.to(self.device)
+
                 predictions = self.model(user_ids, item_ids)
                 loss = criterion(predictions, labels)
                 
