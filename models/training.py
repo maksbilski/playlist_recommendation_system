7f9aad5a2a3b8173daa16a6195945a0dc0d90caa
mrozek <01178582@pw.edu.pl>
2025-01-06 23:56:14 +0100

    IMPLEMENT: Added training and validation epoch loss log

--- models/training.py
+++ models/training.py
@@ -1,36 +1,56 @@
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 
 
 class GMFTrainer:
-    def __init__(self, model, train_loader, device):
+    def __init__(self, model, train_loader, val_loader, device):
         self.model = model
         self.train_loader = train_loader
+        self.val_loader = val_loader
         self.device = device
 
     def train_with_weight_decay(self, epochs, learning_rate, weight_decay=0.01):
         self.model = self.model.to(self.device)
         criterion = nn.MSELoss()
         optimizer = optim.AdamW(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-
+        epoch_train_losses, epoch_val_losses = [], []
+        
         for epoch in range(epochs):
+            train_losses, val_losses = [], []
             self.model.train()
-            total_loss = 0
-            
+
             for user_ids, item_ids, labels in self.train_loader:
                 user_ids = user_ids.to(self.device)
                 item_ids = item_ids.to(self.device)
                 labels = labels.to(self.device)
 
                 predictions = self.model(user_ids, item_ids)
-                loss = criterion(predictions, labels)
-                
+                train_loss = criterion(predictions, labels)
+                train_losses.append(train_loss.item())
+
                 optimizer.zero_grad()
-                loss.backward()
+                train_loss.backward()
                 optimizer.step()
-                
-                total_loss += loss.item()
-                
-            avg_loss = total_loss / len(self.train_loader)
-            print(f'Epoch {epoch+1}/{epochs} - Average Loss: {avg_loss:.4f}')
+
+            self.model.eval()
+            for user_ids, item_ids, labels in self.val_loader:
+                user_ids = user_ids.to(self.device)
+                item_ids = item_ids.to(self.device)
+                labels = labels.to(self.device)
+                with torch.no_grad():
+                    predictions = self.model(user_ids, item_ids)
+
+                val_loss = criterion(predictions, labels)
+                val_losses.append(val_loss.item())
+
+            epoch_train_loss = np.mean(train_losses)
+            epoch_val_loss = np.mean(val_losses)
+            epoch_train_losses.append(epoch_train_loss)
+            epoch_val_losses.append(epoch_val_loss)
+        return epoch_train_losses, epoch_val_losses
+
+            
+
+
