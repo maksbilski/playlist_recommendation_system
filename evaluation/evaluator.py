17aa98898f3b804900866e88aa2565256e3030ad
mrozek <01178582@pw.edu.pl>
2025-01-06 23:58:43 +0100

    REFACTOR: Updated the evaluator class so it works fine with validation script

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -6,38 +6,28 @@ class Evaluator:
         self.k_values = k_values
         self.relevance_threshold = relevance_threshold 
 
-    def evaluate(self,
-                 predictions_df,
-                 ground_truth_df):
-        merged_df = predictions_df.merge(
-            ground_truth_df,
-            on=['user_id', 'track_id'],
-            how='right',
-            suffixes=('_pred', '')
-        ).fillna({'score': 0.0})
-        
-        print(merged_df)
-
+    def evaluate(self, pred_sessions_df):
         metrics = {}
-
         for k in self.k_values:
-#            ndcg_scores = merged_df.groupby('user_id').apply(
+#            ndcg_scores = pred_sessions_df.groupby('user_id').apply(
 #                lambda x: calculate_ndcg(x, k)   
 #            )
 
-            recall_scores = merged_df.groupby('user_id').apply(
+            recall_scores = pred_sessions_df.groupby('user_id').apply(
                 lambda x: calculate_recall(x, k, self.relevance_threshold)
             )
 
-            precision_scores = merged_df.groupby('user_id').apply(
+            precision_scores = pred_sessions_df.groupby('user_id').apply(
                 lambda x: calculate_precision(x, k, self.relevance_threshold)
             )
 
 #            metrics[f'NDCG@{k}'] = ndcg_scores.mean()
 #            metrics[f'NDCG@{k}_std'] = ndcg_scores.std()
-            metrics[f'Recall@{k}'] = recall_scores.mean()
+            metrics[f'Recall@{k}'] = recall_scores
+            metrics[f'Recall@{k}_mean'] = recall_scores.mean()
             metrics[f'Recall@{k}_std'] = recall_scores.std()
-            metrics[f'Precision@{k}'] = precision_scores.mean()
+            metrics[f'Precision@{k}'] = precision_scores
+            metrics[f'Precision@{k}_mean'] = precision_scores.mean()
             metrics[f'Precision@{k}_std'] = precision_scores.std()
 
         return metrics
