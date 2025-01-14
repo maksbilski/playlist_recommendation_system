1dfa7a05b9062238b96fc5a08a27b353946b8dcb
mrozek <01178582@pw.edu.pl>
2025-01-14 21:39:14 +0100

    Moved wmf to advanced model

--- models/advanced_model.py
+++ models/advanced_model.py
@@ -1,30 +1,21 @@
-import torch.nn as nn
 import torch
+import torch.nn as nn
 
-class MLP(nn.Module):
-    def __init__(self, n_users, n_items, embedding_dim=16, hid_layers_dims=[32, 16, 8], dropout_rate=0.2, init=False):
-        super(MLP, self).__init__()
-        
+
+class GMF(nn.Module):
+    def __init__(self, n_users, n_items, embedding_dim=8, dropout_rate=0.2, init=False, bias=False, sigmoid=False):
+        super(GMF, self).__init__()
+
+        self.bias = bias
+        self.sigmoid = sigmoid
         self.user_embedding = nn.Embedding(num_embeddings=n_users, embedding_dim=embedding_dim)
         self.item_embedding = nn.Embedding(num_embeddings=n_items, embedding_dim=embedding_dim)
-
-        layers = []
-        input_dim = embedding_dim * 2
-
-        for dim in hid_layers_dims:
-            layers.extend([
-                nn.Linear(input_dim, dim),
-                nn.BatchNorm1d(dim),
-                nn.ReLU(),
-                nn.Dropout(dropout_rate)
-            ])
-            input_dim = dim
-
-        layers.append(nn.Linear(hid_layers_dims[-1], 1))
-
-        self.layers = nn.Sequential(*layers)
-
         self.dropout = nn.Dropout(dropout_rate)
+
+        if bias:
+            self.user_bias = nn.Parameter(torch.zeros(n_users))
+            self.item_bias = nn.Parameter(torch.zeros(n_items))
+            self.offset = nn.Parameter(torch.zeros(1))
 
         if init:
             self.user_embedding.weight.data.uniform_(-0.1, 0.1)
@@ -34,9 +25,12 @@ class MLP(nn.Module):
         user_embedded = self.dropout(self.user_embedding(user_input))
         item_embedded = self.dropout(self.item_embedding(item_input))
 
-        vector = torch.cat([user_embedded, item_embedded], dim=-1)
+        element_product = (user_embedded * item_embedded).sum(dim=1)
 
-        output = self.layers(vector)
+        if self.bias:
+            element_product += self.user_bias[user_input] + self.item_bias[item_input] + self.offset
 
-        return output.squeeze()
+        if self.sigmoid:
+            element_product = torch.sigmoid(element_product)
 
+        return element_product.squeeze()
