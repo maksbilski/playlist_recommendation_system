499a6d8921d5efc84906734922cfbd9238b73e6c
mrozek <01178582@pw.edu.pl>
2025-01-15 22:17:15 +0100

    FIX: removed unnecessary unpack variables

--- prediction/predict.py
+++ prediction/predict.py
@@ -4,10 +4,9 @@ def predict(model, data_loader, device):
     model = model.to(device)
     model.eval()
     predictions = []
-    for user_ids, item_ids, labels, weights in data_loader:
+    for user_ids, item_ids in data_loader:
         user_ids = user_ids.to(device)
         item_ids = item_ids.to(device)
-        labels = labels.to(device)
         with torch.no_grad():
             batch_predictions = model(user_ids, item_ids)
         predictions.append(batch_predictions.cpu())
