af257ada66bde4b6919423aacc3a3f6a4f7ce054
mrozek <01178582@pw.edu.pl>
2025-01-16 04:25:47 +0100

    REFACTOR: Added types in methods and uncommented NDCG section since it works now

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -1,17 +1,19 @@
+from typing import Dict, List
+import pandas as pd
 from .metrics import calculate_ndcg, calculate_recall, calculate_precision
 
 
 class Evaluator:
-    def __init__(self, k_values, relevance_threshold):
+    def __init__(self, k_values: List[int], relevance_threshold: float):
         self.k_values = k_values
         self.relevance_threshold = relevance_threshold 
 
-    def evaluate(self, pred_sessions_df):
+    def evaluate(self, pred_sessions_df: pd.DataFrame) -> Dict:
         metrics = {}
         for k in self.k_values:
-#            ndcg_scores = pred_sessions_df.groupby('user_id').apply(
-#                lambda x: calculate_ndcg(x, k)   
-#            )
+            ndcg_scores = pred_sessions_df.groupby('user_id').apply(
+                lambda x: calculate_ndcg(x, k)   
+            )
 
             recall_scores = pred_sessions_df.groupby('user_id').apply(
                 lambda x: calculate_recall(x, k, self.relevance_threshold)
@@ -21,8 +23,9 @@ class Evaluator:
                 lambda x: calculate_precision(x, k, self.relevance_threshold)
             )
 
-#            metrics[f'NDCG@{k}'] = ndcg_scores.mean()
-#            metrics[f'NDCG@{k}_std'] = ndcg_scores.std()
+            metrics[f'NDCG@{k}'] = ndcg_scores
+            metrics[f'NDCG@{k}_mean'] = ndcg_scores.mean()
+            metrics[f'NDCG@{k}_std'] = ndcg_scores.std()
             metrics[f'Recall@{k}'] = recall_scores
             metrics[f'Recall@{k}_mean'] = recall_scores.mean()
             metrics[f'Recall@{k}_std'] = recall_scores.std()
